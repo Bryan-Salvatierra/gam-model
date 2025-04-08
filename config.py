4345622989d7c6c3fa336681a1a03c930df8a734
Bryan-Salvatierra <bryan.salvatierra01@gmail.com>
2025-04-08 18:23:52 -0500

    Agregado todos los archivos necesarios

---
+++ config.py
@@ -0,0 +1,49 @@
+import os 
+
+import torch
+import torch.backends.cudnn as cudnn 
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
+
+from model import Discriminator
+from model import Generator
+
+
+torch.manual_seed(0)
+device = torch.device("cuda:0")
+cudnn.benchmark = True
+mode = "train"
+exp_name = "exp000"
+
+if mode == "train":
+    
+    dataset_dir = "data"
+    batch_size  = 128
+    
+    discriminator = Discriminator().to(device)
+    generator = Generator().to(device)
+    
+    start_epoch = 0
+    resume = False
+    resume_d_weight = ""
+    resume_g_weight = ""
+    
+    epochs = 128
+    
+    criterion = nn.BCELoss().to(decive)
+    
+    d_optimizer           = optim.Adam(discriminator.parameters(), 0.0002, (0.5, 0.999))  # Discriminator learning rate during adversarial network training.
+    g_optimizer           = optim.Adam(generator.parameters(),     0.0002, (0.5, 0.999))  # Generator learning rate during adversarial network training.
+
+    write = SummaryWriter(os.path.join("samples", "logs", exp_name))
+    
+    exp_dir1 = os.path.join("samples", exp_name)
+    exp_dir2 = os.path.join("results", exp_name)
+    
+    if mode == "valid":
+        
+        exp_dir = os.path.join("results", "test", exp_name)
+        
+        model = Generator().to(device)
+        model_path = f"samples/{exp_name}/g-last.pth"
