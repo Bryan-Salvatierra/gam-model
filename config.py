e992f57f9e507c7c4a459f2ce1669f86ded0f3ad
Bryan-Salvatierra <bryan.salvatierra01@gmail.com>
2025-04-08 19:00:10 -0500

    Agregado todos los archivos necesarios

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-import os 
-
+import os
 import torch
 import torch.backends.cudnn as cudnn 
 import torch.nn as nn
@@ -9,7 +8,6 @@ from torch.utils.tensorboard import SummaryWriter
 from model import Discriminator
 from model import Generator
 
-
 torch.manual_seed(0)
 device = torch.device("cuda:0")
 cudnn.benchmark = True
@@ -17,7 +15,6 @@ mode = "train"
 exp_name = "exp000"
 
 if mode == "train":
-    
     dataset_dir = "data"
     batch_size  = 128
     
@@ -31,19 +28,17 @@ if mode == "train":
     
     epochs = 128
     
-    criterion = nn.BCELoss().to(decive)
+    criterion = nn.BCELoss().to(device)
     
-    d_optimizer           = optim.Adam(discriminator.parameters(), 0.0002, (0.5, 0.999))  # Discriminator learning rate during adversarial network training.
-    g_optimizer           = optim.Adam(generator.parameters(),     0.0002, (0.5, 0.999))  # Generator learning rate during adversarial network training.
+    d_optimizer = optim.Adam(discriminator.parameters(), 0.0002, (0.5, 0.999))
+    g_optimizer = optim.Adam(generator.parameters(), 0.0002, (0.5, 0.999))
 
-    write = SummaryWriter(os.path.join("samples", "logs", exp_name))
+    writer = SummaryWriter(os.path.join("samples", "logs", exp_name))
     
     exp_dir1 = os.path.join("samples", exp_name)
     exp_dir2 = os.path.join("results", exp_name)
-    
-    if mode == "valid":
-        
-        exp_dir = os.path.join("results", "test", exp_name)
-        
-        model = Generator().to(device)
-        model_path = f"samples/{exp_name}/g-last.pth"
+
+elif mode == "valid":
+    exp_dir = os.path.join("results", "test", exp_name)
+    model = Generator().to(device)
+    model_path = f"samples/{exp_name}/g-last.pth"
--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from torch import Tensor
 
 __all__ = [
-    "Discrminator", "Generator"
+    "Discriminator", "Generator"
 ]
 
 class Discriminator(nn.Module):
@@ -16,40 +16,33 @@ class Discriminator(nn.Module):
             nn.LeakyReLU(0.2, True),
             nn.Linear(256, 1, bias=True),
             nn.Sigmoid()
-      
         )
-        
-        def forward(self, x: Tensor) -> Tensor:
-            out = torch.flatten(x, 1)
-            out = self.main(out)
-            
-            return out
+    
+    def forward(self, x: Tensor) -> Tensor:
+        out = torch.flatten(x, 1)
+        out = self.main(out)
+        return out
         
 class Generator(nn.Module):
     def __init__(self) -> None:
         super(Generator, self).__init__()
         self.main = nn.Sequential(
-                        nn.Linear(100, 128, bias=True),
+            nn.Linear(100, 128, bias=True),
             nn.LeakyReLU(0.2, True),
-
             nn.Linear(128, 256, bias=False),
             nn.BatchNorm1d(256),
             nn.LeakyReLU(0.2, True),
-
             nn.Linear(256, 512, bias=False),
             nn.BatchNorm1d(512),
             nn.LeakyReLU(0.2, True),
-
             nn.Linear(512, 1024, bias=False),
             nn.BatchNorm1d(1024),
             nn.LeakyReLU(0.2, True),
-
-            # Output: channels * image size * image size.
             nn.Linear(1024, 1 * 28 * 28, bias=True),
             nn.Tanh()
         )
-        def forward(self, x: Tensor) -> Tensor:
-            out = self.main(x)
-            out = out.reshape(out.size(0), 1, 28, 28)
-            
-            return out
+    
+    def forward(self, x: Tensor) -> Tensor:
+        out = self.main(x)
+        out = out.reshape(out.size(0), 1, 28, 28)
+        return out
